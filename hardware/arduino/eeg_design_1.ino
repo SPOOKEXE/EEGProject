11f61e15e2d4a62e3e06bdd435e683d919b34e7b
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2023-09-15 08:59:28 +0930

    cleanup

---
+++ hardware/arduino/eeg_design_1.ino
@@ -0,0 +1,25 @@
+
+
+/*
+
+	EEG Design 1
+	> 64 electrodes
+	> 1 output, single stream
+
+	Pros:
+	- customization for node positions
+	- automate changing electrode charges via software
+
+	Cons:
+	- slow
+	- complex
+
+*/
+
+void setup() {
+
+}
+
+void start() {
+
+}
---
+++ hardware/arduino/eeg_design_2.ino
@@ -0,0 +1,25 @@
+
+/*
+
+	EEG Design 2
+	> 64 electrodes
+	> 1 output, single stream
+
+	Pros:
+	- fast
+	- efficient
+	- simple
+
+	Cons:
+	- limited (software) customization
+	- manual node positions via hardware
+
+*/
+
+void setup() {
+	
+}
+
+void start() {
+
+}
